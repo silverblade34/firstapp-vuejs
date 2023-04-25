b53e0e91f729e4609a12fa87ac793c79a7d6b12a
Michell Pacheco Tacay <90942815+silverblade34@users.noreply.github.com>
2023-04-25 17:53:04 -0500

    primer avance

---
+++ src/components/PostButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button class="btn-acceder">ACCEDER</button>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.btn-acceder{
+    padding: 10px 15px;
+    border-radius: 8px;
+    background-color: brown;
+    color: #ffff;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Crear una aplicacion Vue y la junta en el index html <div id="app">
 createApp(App).mount('#app')
